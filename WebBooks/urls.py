690879070a957989198276581ca185dfa5e09ed9
OIITuMuCT <vataman@live.ru>
2024-11-12 15:49:45 +0300

    Добавление картинок в админ панель для книг, авторов

--- WebBooks/urls.py
+++ WebBooks/urls.py
@@ -16,10 +16,17 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from catalog import views
+# для показа картинок
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.index, name="index"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                         document_root=settings.MEDIA_ROOT)
--- catalog/admin.py
+++ catalog/admin.py
@@ -1,21 +1,50 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
+
 from .models import Author, Book, Genre, Language, Publisher, Status, BookInstance
 
 # Определяем класс AuthorAdmin для авторов книг
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('last_name', 'first_name')
+    list_display = ('last_name', 'first_name', 'photo', 'show_photo')
     fields = ['last_name', 'first_name', ('date_of_birth', 'photo')]
+    readonly_fields = ["show_photo"]
+    def show_photo(self, obj):
+        # return format_html(
+        #     f'<img src="{obj.photo.url}" style="max-height: 100px;">'
+        # )
+        # можно и с использованием функции mark_safe
+        return mark_safe(
+            f'<img src="{obj.photo.url}" style="max-height: 100px;">'
+        )
+    show_photo.short_description = 'Фото'
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'genre', 'language', 'display_author')
+    list_display = ('title', 'genre', 'language', 'display_author', 'show_photo')
     list_filter = ('genre', 'author')
-
+    inlines = [BookInstanceInline]
+    readonly_fields = ["show_photo"]
+    def show_photo(self, obj):
+        return format_html(
+            f'<img src="{obj.photo.url}" style="max-height: 100px;">'
+        )
+    show_photo.short_description = 'Обложка'
 # регистрируем класс  BookInstanceAdmin для экземпляра книг
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('book', 'status')
-
+    fieldsets = (
+        ('Экземпляр книги', {
+            'fields': ('book', 'inv_nom')
+        }),
+        ('Статус и окончания его действия', {
+            'fields' : ('status', 'due_back')
+        }),
+    )
 
 
 # Register your models here.
@@ -24,5 +53,3 @@ admin.site.register(Genre)
 admin.site.register(Language)
 admin.site.register(Publisher)
 admin.site.register(Status)
-
-
