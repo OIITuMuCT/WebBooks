6f5b10f1ba71c97081704f135dff71771a7f0f35
OIITuMuCT <vataman@live.ru>
2024-11-12 16:53:34 +0300

    Создание шаблона сайта base, index

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+
 def index(request):
-    return HttpResponse('Главная страница сайта "Мир книг"!')
+    text_head = 'Это заголовок главной страницы сайта'
+    text_body = 'Это содержимое главной страницы сайта'
+    context = {'text_head': text_head, 'text_body': text_body}
+    # передача словаря context с данным в шаблон
+    return render(request, 'catalog/index.html', context)
